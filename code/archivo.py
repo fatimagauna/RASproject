224eee35dd12a808cf954325fedfedf4c89c6b1c
fatimagauna <87443052+fatimagauna@users.noreply.github.com>
2021-07-21 19:42:09 -0500

    Update archivo.py

--- code/archivo.py
+++ code/archivo.py
@@ -10,22 +10,18 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(dirA1,GPIO.OUT)
 GPIO.setup(dirA2,GPIO.OUT)
 GPIO.setup(spdA,GPIO.OUT)
-pwmA=GPIO.PWM(spdA,100)
+pwmA=GPIO.PWM(spdA,1000)
 pwmA.start(10)
 
-try:
-    
-    while(1):
-        GPIO.output(dirA1,True)
-        GPIO.output(dirA2,False)
-        pwmA.ChangeDutyCycle(25)
-        time.sleep(5)
-        GPIO.output(dirA1,False)
-        GPIO.output(dirA2,True)
-        pwmA.ChangeDutyCycle(10)
-        time.sleep(5)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-        
+while(1):
+    GPIO.output(dirA1,True)
+    GPIO.output(dirA2,False)
+    pwmA.ChangeDutyCycle(25)
+    time.sleep(5)
+    GPIO.output(dirA1,False)
+    GPIO.output(dirA2,True)
+    pwmA.ChangeDutyCycle(10)
+    time.sleep(5)
+
 #Always at the end of the code
 GPIO.cleanup()
