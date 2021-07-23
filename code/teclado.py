d514a53645bd891826a28612de2458fa60ae2165
fatimagauna <87443052+fatimagauna@users.noreply.github.com>
2021-07-22 20:01:14 -0500

    Update teclado.py

--- code/teclado.py
+++ code/teclado.py
@@ -50,33 +50,33 @@ while(1):
     cv2.imshow('Video', frame)
 
     if ((cv2.waitKey(33) == -1) and (x == 1)):
-	print ("Ninguna")
-	x = 0
+		print ("Ninguna")
+		x = 0
 
     if (cv2.waitKey(33) == ord('w')):
-	print ("Upkey")
-	x = 1
-	adelante()
+		print ("Upkey")
+		x = 1
+		adelante()
 
 
     if (cv2.waitKey(33) == ord('a')):
-	print ("Leftkey")
-	x = 1
+		print ("Leftkey")
+		x = 1
 
     if (cv2.waitKey(33) == ord('s')):
-	print ("Dowkey")
-	x = 1
-	atras()
+		print ("Dowkey")
+		x = 1
+		atras()
 
 
     if (cv2.waitKey(33) == ord('d')):
-	print ("Rightkey")
-	x = 1
+		print ("Rightkey")
+		x = 1
 
     if (cv2.waitKey(33) == ord('q')):
-	cap.release()
-	cv2.destroyAllWindows()
-	break
+		cap.release()
+		cv2.destroyAllWindows()
+		break
 		
 
 #Always at the end of the code
